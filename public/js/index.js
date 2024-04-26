fcf38e9eccd8faab3a027db15dc959d689b1704f
justinwilliams.dev <justinpwilliams@duck.com>
2024-04-26 09:49:48 -0500

    track public folder since I'm not building js yet

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+document.body.addEventListener('showRoster', handleShowEvent)
+document.body.addEventListener('downloadCsv', handleDownloadFile)
+
+/* Shows the roster after the first player is added.
+ * @returns { void } 
+ */
+function handleShowEvent() {
+    var node = document.querySelector('.roster-table');
+    if (node.classList.contains('hidden')) {
+        node.classList.remove('hidden');
+    }
+};
+
+/* Open a new tab/window to download the generated CSV file
+ * @returns { void }
+ */
+function handleDownloadFile(event) {
+    console.log(event.detail);
+    window.open(`/download/${event.detail.TeamName}-${event.detail.Id}`, '_blank').focus();
+}
